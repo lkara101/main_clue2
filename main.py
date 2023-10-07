e58303aff22a1d349d71af5d1a22942a8d102c46
Lydia Kara <lkara4@bloomberg.net>
2023-10-07 23:08:23 +0100

    connected to mongodb cluster1

--- main.py
+++ main.py
@@ -1,16 +1,4 @@
-# This is a sample Python script.
+# main thing .py baby!
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print('Have i forgotten everything wow, fuck me')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with open("mongoo.py") as f:
+    exec(f.read())
